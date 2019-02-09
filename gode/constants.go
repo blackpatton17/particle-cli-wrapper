7d8b8ab00588ae6928fa45c29801be190f643247
Julien Vanier <julien@particle.io>
2019-02-08 20:51:34 -0500

    Revert to Node 8.15.0 since serialport 6.2.0 is not prebuilt for Node 10

--- gode/constants.go
+++ gode/constants.go
@@ -7,7 +7,7 @@ package gode
 //
 
 // Version is the requested node version
-const Version = "10.15.1"
+const Version = "8.15.0"
 
 // NpmVersion is the requested npm version
 const NpmVersion = "6.4.1"
@@ -16,9 +16,9 @@ const npmSha = "c99ed6d004316baa5a4e9788d86f0448e49dc0dd6b9ff7a75d805ace609a2052
 const npmURL = "https://github.com/npm/cli/archive/v6.4.1.zip"
 
 var targets = []Target{
-	{"amd64", "linux", "https://binaries.particle.io/node/v10.15.1/node-v10.15.1-linux-x64.tar.gz", "node-v10.15.1-linux-x64", "ca1dfa9790876409c8d9ecab7b4cdb93e3276cedfc64d56ef1a4ff1778a40214"},
-	{"arm", "linux", "https://binaries.particle.io/node/v10.15.1/node-v10.15.1-linux-armv7l.tar.gz", "node-v10.15.1-linux-armv7l", "1cef71ded0411a809795a1f001fbafc48f04f9e361069a9764f530f32af7ecf0"},
-	{"amd64", "darwin", "https://binaries.particle.io/node/v10.15.1/node-v10.15.1-darwin-x64.tar.gz", "node-v10.15.1-darwin-x64", "327dcef4b61dead1ae04d2743d3390a2b7e6cc6c389c62cfcfeb0486c5a9f181"},
-	{"386", "windows", "https://binaries.particle.io/node/v10.15.1/win-x86/node.exe", "node-v10.15.1-windows-x86", "c6427a5f8ab6d0e785b31e360f9076c602271e53da06199ad5cc253e0e0fd401"},
-	{"amd64", "windows", "https://binaries.particle.io/node/v10.15.1/win-x64/node.exe", "node-v10.15.1-windows-x64", "b4783f5b54e9c8dd84937dd6bd07a9f46a01369bf76235cbcce4315ef3103c31"},
+	{"amd64", "linux", "https://binaries.particle.io/node/v8.15.0/node-v8.15.0-linux-x64.tar.gz", "node-v8.15.0-linux-x64", "dc004e5c0f39c6534232a73100c194bc1446f25e3a6a39b29e2000bb3d139d52"},
+	{"arm", "linux", "https://binaries.particle.io/node/v8.15.0/node-v8.15.0-linux-armv7l.tar.gz", "node-v8.15.0-linux-armv7l", "a19b38d89f87c357569ee6ec99e767789a97c1a053e073676cfb0d68945d584e"},
+	{"amd64", "darwin", "https://binaries.particle.io/node/v8.15.0/node-v8.15.0-darwin-x64.tar.gz", "node-v8.15.0-darwin-x64", "a393971136408f837fbc0f7d71a63754f91cfb1851d48bd612d8219eb61956f1"},
+	{"386", "windows", "https://binaries.particle.io/node/v8.15.0/win-x86/node.exe", "node-v8.15.0-windows-x86", "94ee364516cd3603b0673afee7f190a38439e797136fa425aa593ce2d0ef48c2"},
+	{"amd64", "windows", "https://binaries.particle.io/node/v8.15.0/win-x64/node.exe", "node-v8.15.0-windows-x64", "f0cd491948c4a23fe93ea9db694ffff66e20298552f00268d009e25e96cd3482"},
 }
